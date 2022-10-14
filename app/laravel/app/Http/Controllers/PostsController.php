2515f2d5b824cc2ae1314b2a762b4fed7ade61ce
mako522 <chori0501.ma@gmail.com>
2022-10-14 19:14:43 +0900

    a

--- app/laravel/app/Http/Controllers/PostsController.php
+++ app/laravel/app/Http/Controllers/PostsController.php
@@ -63,27 +63,24 @@ class PostsController extends Controller
         $like = new Like;
         $review = Review::findOrFail($review_id);
 
-        // 空でない（既にいいねしている）なら
+        
         if ($like->like_exist($id, $review_id)) {
-            //likesテーブルのレコードを削除
+            
             $like = Like::where('review_id', $review_id)->where('user_id', $id)->delete();
         } else {
-            //空（まだ「いいね」していない）ならlikesテーブルに新しいレコードを作成する
+            
             $like = new Like;
             $like->review_id = $request->review_id;
             $like->user_id = Auth::user()->id;
             $like->save();
         }
-
-        //loadCountとすればリレーションの数を○○_countという形で取得できる（今回の場合はいいねの総数）
         $reviewLikesCount = $post->loadCount('likes')->likes_count;
 
-        //一つの変数にajaxに渡す値をまとめる
-        //今回ぐらい少ない時は別にまとめなくてもいいけど一応。笑
+        
         $json = [
             'reviewLikesCount' => $reviewLikesCount,
         ];
-        //下記の記述でajaxに引数の値を返す
+        
         return response()->json($json);
     }
 }
--- app/laravel/resources/js/_ajaxlike.js
+++ app/laravel/resources/js/_ajaxlike.js
@@ -1,52 +1,35 @@
-// $(function () {
-//     var like = $('.js-like-toggle');
-//     var likeReviewId;
+$(function () {
+    var like = $('.js-like-toggle');
+    var likeReviewId;
     
-//     $(like).on('click', function () {
-//         var $this = $(this);
-//         console.log($this);
-//         likeReviewId = $this.data('reviewid');
-//         $.ajax({
-//                 headers: {
-//                     'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-//                 },
-//                 url: '/ajaxlike',  
-//                 type: 'POST', 
-//                 data: {
-//                     'review_id': likeReviewId 
-//                 },
-//         })
-    
-//             // Ajaxリクエストが成功した場合
-//             .done(function (data) {
-    
-//                 $this.toggleClass('loved');  
-    
-//             })
-//             // Ajaxリクエストが失敗した場合
-//             .fail(function (data, xhr, err) {
-    
-//                 console.log('エラー');
-//                 console.log(err);
-//                 console.log(xhr);
-//             });
-        
-//         return false;
-//     });
-//     });
+    $(like).on('click', function () {
+        var $this = $(this);
+        console.log($this);
+        likeReviewId = $this.data('reviewid');
+        $.ajax({
+                headers: {
+                    'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                },
+                url: '/ajaxlike',  
+                type: 'POST', 
+                data: {
+                    'review_id': likeReviewId 
+                },
+        })
 
-function like(reviewId) {
-    $.ajax({
-      headers: {
-        "X-CSRF-TOKEN": $('meta[name="csrf-token"]').attr("content"),
-      },
-      url: `/like/${reviewId}`,
-      type: "POST",
-    })
-      .done(function (data, status, xhr) {
-        console.log(data);
-      })
-      .fail(function (xhr, status, error) {
-        console.log();
-      });
-  }
+            .done(function (data) {
+    
+                $this.toggleClass('loved');  
+    
+            })
+            .fail(function (data, xhr, err) {
+    
+                console.log('エラー');
+                console.log(err);
+                console.log(xhr);
+            });
+        
+        return false;
+    });
+    });
+
